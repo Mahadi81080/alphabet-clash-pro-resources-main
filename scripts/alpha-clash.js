6e950caa849d7e719cb30d7f13867646d66b76a6
Mahadi810803 <mahedihasan81080@gmail.com>
2024-02-16 00:14:33 +0600

    complite alpha clash pro

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,16 +1,87 @@
-function play() {
-  const homeSection = document.getElementById("home-screen");
-  homeSection.classList.add("hidden");
-  const playGroundSection = document.getElementById("play-ground");
-  playGroundSection.classList.remove("hidden");
-  // generate a random alphabet
+// function play() {
+//   const homeSection = document.getElementById("home-screen");
+//   homeSection.classList.add("hidden");
+//   const playGroundSection = document.getElementById("play-ground");
+//   playGroundSection.classList.remove("hidden");
+// }
+function handleKeyboardKeyUpEvent(event) {
+  const playerPressed = event.key;
+  console.log("player pressed", playerPressed);
+  // stop the game if press 'Esc'
+  if(playerPressed === 'Escape'){
+    gameOver()
+  }
+  // current expected press
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLowerCase();
+  console.log(playerPressed, expectedAlphabet);
+  // check match or not
+  if (playerPressed === expectedAlphabet) {
+    console.log("you got a point");
+    const currentScore = getTextElementValueById('current-score')
+    const updateScore = currentScore + 1;
+    setTextElementById('current-score', updateScore);
+
+    // .......................option-2................................
+    // update score
+    // const currentScoreElement = document.getElementById('current-score')
+    // const currentScoreText = currentScoreElement.innerText;
+    // const currentScore = parseInt(currentScoreText);
+    // console.log(currentScore);
+    // // increase the score number 1
+    // const newScore = currentScore + 1;
+    // currentScoreElement.innerText= newScore;
+    // .............................................................
+
+    // start a new round
+    removeBackgroundColor(expectedAlphabet);
+    continueGame();
+  } else {
+    console.log("you lose a point");
+    const currentLife = getTextElementValueById('current-life')
+    const updateLife = currentLife - 1;
+    setTextElementById('current-life', updateLife);
+    if(updateLife === 0){
+      gameOver()
+      // update final score
+      const gameScore = getTextElementValueById('current-score');
+      setTextElementById('game-score', gameScore)
+      // clear the last selected alphabet
+      const currentAlphabet = getElementTextById('current-alphabet')
+      removeBackgroundColor(currentAlphabet);
+    }
+     // .......................option-2................................
+    // const currentLifeElement = document.getElementById("current-life");
+    // const currentScoreText = currentLifeElement.innerText;
+    // const currentLife = parseInt(currentScoreText);
+    // const newLife = currentLife - 1;
+    // currentLifeElement.innerText = newLife;
+    // ................................................................
+  }
+}
+// capture keyboard key press
+document.addEventListener("keyup", handleKeyboardKeyUpEvent);
+
+function continueGame() {
+  // step 1 :generate a random alphabet
   const alphabet = getRandomAlphabet();
-  console.log("Your Random Alphabet:", alphabet);
   //    set randomly generate alphabet to the screen(show it)
-  const currentAlphabetElement = document.getElementById('current-alphabet');
+  const currentAlphabetElement = document.getElementById("current-alphabet");
   currentAlphabetElement.innerText = alphabet;
-
-//   set background color
- setBackgroundColor(alphabet);
-
+  //   set background color
+  setBackgroundColor(alphabet);
+}
+function play() {
+  hideElementById("home-screen");
+  hideElementById("score");
+  showElementById("play-ground");
+  // reset score and life
+  setTextElementById('current-life', 5)
+  setTextElementById('current-score', 0)
+  continueGame();
+}
+function gameOver(){
+  hideElementById("play-ground");
+  showElementById("score");
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,18 +1,43 @@
-function getRandomAlphabet(){
-    const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
-    const alphabets = alphabetString.split('');
-
-    // Get random index between 0-25
-    const randomNumber = Math.random()*25;
-    const index = Math.round(randomNumber);
-
-    const alphabet = alphabets[index];
-    // console.log(alphabet,index);
-    return alphabet;
-
+function hideElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("hidden");
 }
+function showElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("hidden");
+}
+function setBackgroundColor(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-orange-400");
+}
+function removeBackgroundColor(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-orange-400");
+}
+function getTextElementValueById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValue = element.innerText;
+  const value = parseInt(elementValue);
+  return value;
+}
+function setTextElementById(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text =element.innerText;
+    return text;
+}
+function getRandomAlphabet() {
+  const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+  const alphabets = alphabetString.split("");
 
-function setBackgroundColor(elementId){
-     const element = document.getElementById(elementId);
-     element.classList.add('bg-orange-400');
+  // Get random index between 0-25
+  const randomNumber = Math.random() * 25;
+  const index = Math.round(randomNumber);
+
+  const alphabet = alphabets[index];
+  // console.log(alphabet,index);
+  return alphabet;
 }
